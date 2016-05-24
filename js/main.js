6ffe0ec74a2b501327cb45b40422ed58c7f9ec13
jenny-q <jenny.q@me.com>
2016-05-24 13:50:06 -0400

    Modified files
    Modified files to include carousel, menu, fixed spacing issues

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-function myFunction() {
-    document.getElementsByClassName("nav")[0].classList.toggle("responsive");
-}
-
 //carousel function
 $(document).ready(function(){
   $('.lux-hero').slick({
@@ -11,4 +7,26 @@ $(document).ready(function(){
    nextArrow: '<img src="img/icon-left.png" class="left-arrow">',
   prevArrow: '<img src="img/icon-right.png" class="right-arrow">',
   });
+});
+
+//slide out menu
+$('a').on('click',function() {
+    if($('#menu').css('left')=='0px'){
+        $('#menu').animate({left: '-400px'}, 500);        
+    }else{
+        $('#menu').animate({left:0}, 500); 
+    }
+});
+
+//retina ready @2x 
+$(document).ready(function(){
+    if (window.devicePixelRatio > 1) {
+        var lowresImages = $('img')
+
+        images.each(function(i) {
+            var lowres = $(this).attr('src');
+            var highres = lowres.replace(".", "@2x.");
+            $(this).attr('src', highres);
+        });
+    }
 });
